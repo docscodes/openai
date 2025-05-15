ca84c35354baa81e82a4671e2098f8cde8e0d80d
docscodes <docscodes@gmail.com>
2025-05-15 15:49:49 -0400

    add assistant context

---
+++ 01-text/incontext_learning.py
@@ -0,0 +1,16 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    max_tokens=100,
+    temperature=0.5,
+    messages=[{'role': 'system', 'content': 'You are a python programming tutor'},
+              {'role': 'user', 'content': 'Explain what the sum() function does'},
+              {'role': 'assistant', 'content': 'The sum() function returns the '
+               'sum of numbers in the list'},
+              {'role': 'user', 'content': 'Explain what the len() function does'}])
+
+print(response.choices[0].message.content)
